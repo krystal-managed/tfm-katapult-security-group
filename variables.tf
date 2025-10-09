ab4ad2df8ea469e0697dcba88f6ee2e03e3aaeaa
jake.fellows@krystal.uk <jake.fellows@krystal.uk>
2025-10-09 16:29:40 +0100

    Changing to list(string)

--- variables.tf
+++ variables.tf
@@ -1,48 +1,21 @@
 variable "krystal_vpns" {
-  type = list(any)
+  type = list(string)
   default = [
-    {
-      name    = "VPN 1"
-      address = "91.194.74.23"
-    },
-    {
-      name    = "VPN 2"
-      address = "95.131.252.106"
-    },
-    {
-      name    = "Tailscale"
-      address = "100.64.16.0/22"
-    }
+    "91.194.74.23",
+    "95.131.252.106",
+    "100.64.16.0/22"
   ]
 }
 
 variable "zabbix_ips" {
-  type = list(any)
+  type = list(string)
   default = [
-    {
-      name    = "zbx-proxy-ams-01"
-      address = "141.98.24.42"
-    },
-    {
-      name    = "zbx-proxy-lon-01"
-      address = "185.44.252.118"
-    },
-    {
-      name    = "zbx-proxy-ny-01"
-      address = "152.89.76.34"
-    },
-    {
-      name    = "zbx-proxy-pho-01"
-      address = "185.69.56.25"
-    },
-    {
-      name    = "zbx-srv-ams-01"
-      address = "141.98.24.5"
-    },
-    {
-      name    = "zbx-srv-lon-01"
-      address = "77.72.7.10"
-    }
+    "141.98.24.42",
+    "185.44.252.118",
+    "152.89.76.34",
+    "185.69.56.25",
+    "141.98.24.5",
+    "77.72.7.10"
   ]
 }
 
